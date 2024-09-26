48ecee1553e0e5ad7f19c3d11be37f73fdcf4c7b
Paulo Barros <paulo.barros@lambda3.com.br>
2024-09-26 10:28:19 -0300

    test: corrigir nome da env var

--- tests/github.spec.ts
+++ tests/github.spec.ts
@@ -1,56 +1,67 @@
+import {expect, test} from '@playwright/test'
 
-import {expect, test} from '@playwright/test';
-
-const REPO = 'playwright';
-const USER = 'phcbarros';
+const REPO = 'playwright'
+const USER = 'phcbarros'
 
 test.use({
   baseURL: 'https://api.github.com',
   extraHTTPHeaders: {
     // We set this header per GitHub guidelines.
-    'Accept': 'application/vnd.github.v3+json',
+    Accept: 'application/vnd.github.v3+json',
     // Add authorization token to all requests.
     // Assuming personal access token available in the environment.
-    'Authorization': `Bearer ${process.env.API_TOKEN}`,
-    'X-GitHub-Api-Version': '2022-11-28'
+    Authorization: `Bearer ${process.env.GITHUB_API_TOKEN}`,
+    'X-GitHub-Api-Version': '2022-11-28',
   },
 })
 
-test.skip('should create a bug report', {
-  tag: '@github'
-}, async ({ request }) => {
-  const newIssue = await request.post(`/repos/${USER}/${REPO}/issues`, {
-    data: {
-      title: '[Bug] report 1',
-      body: 'Bug description',
-    }
-  });
-  expect(newIssue.ok()).toBeTruthy();
+test(
+  'should create a bug report',
+  {
+    tag: '@github',
+  },
+  async ({request}) => {
+    const newIssue = await request.post(`/repos/${USER}/${REPO}/issues`, {
+      data: {
+        title: '[Bug] report 1',
+        body: 'Bug description',
+      },
+    })
+    expect(newIssue.ok()).toBeTruthy()
 
-  const issues = await request.get(`/repos/${USER}/${REPO}/issues`);
-  console.log(issues)
-  expect(issues.ok()).toBeTruthy();
-  expect(await issues.json()).toContainEqual(expect.objectContaining({
-    title: '[Bug] report 1',
-    body: 'Bug description'
-  }));
-});
+    const issues = await request.get(`/repos/${USER}/${REPO}/issues`)
+    console.log(issues)
+    expect(issues.ok()).toBeTruthy()
+    expect(await issues.json()).toContainEqual(
+      expect.objectContaining({
+        title: '[Bug] report 1',
+        body: 'Bug description',
+      }),
+    )
+  },
+)
 
-test.skip('should create a feature request',{
-  tag: '@github'
-}, async ({ request }) => {
-  const newIssue = await request.post(`/repos/${USER}/${REPO}/issues`, {
-    data: {
-      title: '[Feature] request 1',
-      body: 'Feature description',
-    }
-  });
-  expect(newIssue.ok()).toBeTruthy();
+test.skip(
+  'should create a feature request',
+  {
+    tag: '@github',
+  },
+  async ({request}) => {
+    const newIssue = await request.post(`/repos/${USER}/${REPO}/issues`, {
+      data: {
+        title: '[Feature] request 1',
+        body: 'Feature description',
+      },
+    })
+    expect(newIssue.ok()).toBeTruthy()
 
-  const issues = await request.get(`/repos/${USER}/${REPO}/issues`);
-  expect(issues.ok()).toBeTruthy();
-  expect(await issues.json()).toContainEqual(expect.objectContaining({
-    title: '[Feature] request 1',
-    body: 'Feature description'
-  }));
-});
+    const issues = await request.get(`/repos/${USER}/${REPO}/issues`)
+    expect(issues.ok()).toBeTruthy()
+    expect(await issues.json()).toContainEqual(
+      expect.objectContaining({
+        title: '[Feature] request 1',
+        body: 'Feature description',
+      }),
+    )
+  },
+)
